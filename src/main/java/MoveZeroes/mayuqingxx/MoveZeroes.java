9fa094cee09529ed4ea67e365a4e5472eaee218d
mayuqingxx <37828491+mayuqingxx@users.noreply.github.com>
2019-05-29 09:35:16 +0800

    Update MoveZeroes.java

--- src/main/java/MoveZeroes/mayuqingxx/MoveZeroes.java
+++ src/main/java/MoveZeroes/mayuqingxx/MoveZeroes.java
@@ -1,1 +1,13 @@
 
+class Solution {
+    public void moveZeroes(int[] nums) {
+         for (int i = 0, a = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[a];
+                nums[a++] = temp;
+            }
+        }
+    
+    }
+}
