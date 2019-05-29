af388a129d6e6e5444f4f43102a3aa2152e7ae0a
wangyuhui <wtchange@163.com>
2019-05-29 13:55:48 +0800

    alter algorithm

--- src/main/java/MoveZeroes/wangyuhuich/MoveZeroes.java
+++ src/main/java/MoveZeroes/wangyuhuich/MoveZeroes.java
@@ -17,18 +17,39 @@ package MoveZeroes.wangyuhuich;
  * 解题思路:类似冒泡,数字0 上浮
  */
 
-/**
- * 执行用时 : 122 ms, 在Move Zeroes的Java提交中击败了5.07% 的用户
- * 内存消耗 : 40.6 MB, 在Move Zeroes的Java提交中击败了54.19% 的用户
- */
+
 public class MoveZeroes {
+    /**
+     * 执行用时 : 1 ms, 在Move Zeroes的Java提交中击败了97.69% 的用户
+     * 内存消耗 : 39.9 MB, 在Move Zeroes的Java提交中击败了72.80% 的用户
+     */
     public void moveZeroes(int[] nums) {
+        int zeroCount = 0;
+        int notZeroPosition = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length-i-1; j++) {
-                if (nums[j] == 0 && nums[j + 1] != 0) {
-                    nums[j] = nums[j + 1] + (nums[j + 1] = nums[j]) - nums[j];
-                }
+            if(nums[i]==0){
+                zeroCount+=1;
+            }else{
+                nums[notZeroPosition++] = nums[i];
             }
         }
+        for (int i = notZeroPosition,j = 0; j < zeroCount; i++,j++) {
+            nums[i] = 0;
+        }
     }
+
+    /**
+     * 执行用时 : 122 ms, 在Move Zeroes的Java提交中击败了5.07% 的用户
+     * 内存消耗 : 40.6 MB, 在Move Zeroes的Java提交中击败了54.19% 的用户
+     */
+//    @Deprecated
+//    public void moveZeroes(int[] nums) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 0; j < nums.length-i-1; j++) {
+//                if (nums[j] == 0 && nums[j + 1] != 0) {
+//                    nums[j] = nums[j + 1] + (nums[j + 1] = nums[j]) - nums[j];
+//                }
+//            }
+//        }
+//    }
 }
