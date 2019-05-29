58f9a6575ea0687c58e0dad4488dedcb48c7fb52
ZTEer <365165766@qq.com>
2019-05-29 14:47:43 +0800

    Update MoveZeroes.java
    use stack

--- src/main/java/MoveZeroes/L07152055/MoveZeroes.java
+++ src/main/java/MoveZeroes/L07152055/MoveZeroes.java
@@ -1,1 +1,36 @@
 
+package com.zte;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class MoveZeroes {
+    public static void movezeroes() {
+        Stack<String> stack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        String nums = scanner.next();
+        String[] num = nums.substring(1,nums.length()-1).split(",");
+        StringBuffer result = new StringBuffer();
+        result.append("[");
+        for(int i = 0; i < num.length; i++) {
+            if(!num[i].equals("0")) {
+                result.append(num[i] + ",");
+            } else {
+                stack.push(num[i]);
+            }
+        }
+        while(!stack.empty()) {
+            result.append(stack.peek() + ",");
+            stack.pop();
+        }
+
+        result.replace(result.length()-1, result.length(), "]");
+
+        System.out.println(result.toString());
+    }
+
+    public static void main(String[] args) {
+        movezeroes();
+    }
+}
