5f2be2d2382a3018c467be8fa1398fdb6bb1a3f7
ZTEer <365165766@qq.com>
2019-05-29 16:06:40 +0800

    Update MoveZeroes.java

--- src/main/java/MoveZeroes/L07152055/MoveZeroes.java
+++ src/main/java/MoveZeroes/L07152055/MoveZeroes.java
@@ -1,36 +1,11 @@
-
-package com.zte;
-
-
-import java.util.Scanner;
-import java.util.Stack;
-
 public class MoveZeroes {
-    public static void movezeroes() {
-        Stack<String> stack = new Stack<>();
-        Scanner scanner = new Scanner(System.in);
-        String nums = scanner.next();
-        String[] num = nums.substring(1,nums.length()-1).split(",");
-        StringBuffer result = new StringBuffer();
-        result.append("[");
-        for(int i = 0; i < num.length; i++) {
-            if(!num[i].equals("0")) {
-                result.append(num[i] + ",");
-            } else {
-                stack.push(num[i]);
+    public static void movezeroes(int[] nums) {
+        for(int i = 0, j = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = tmp;
             }
         }
-        while(!stack.empty()) {
-            result.append(stack.peek() + ",");
-            stack.pop();
-        }
-
-        result.replace(result.length()-1, result.length(), "]");
-
-        System.out.println(result.toString());
-    }
-
-    public static void main(String[] args) {
-        movezeroes();
     }
 }
