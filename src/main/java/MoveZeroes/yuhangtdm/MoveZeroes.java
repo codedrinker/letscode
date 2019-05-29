17ad4fcb09f1920c3ac4fbb2642921a392c4edac
yuhang <649411629@qq.com>
2019-05-29 13:47:42 +0800

    去除过多的异常处理

--- src/main/java/MoveZeroes/yuhangtdm/MoveZeroes.java
+++ src/main/java/MoveZeroes/yuhangtdm/MoveZeroes.java
@@ -15,9 +15,6 @@ package MoveZeroes.yuhangtdm;
 public class MoveZeroes {
 
     public static void moveZeroes(Integer[] nums){
-        if (nums == null || nums.length == 0){
-            return;
-        }
         for (int i=0,cur=0; i< nums.length; i++){
             if (nums[i] != 0){
                 int tmp = nums[i];
