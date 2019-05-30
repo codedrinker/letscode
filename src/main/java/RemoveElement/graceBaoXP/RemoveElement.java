5c2e449b0d757144f55075756db257a41e7be0ba
gracebxp <gracebxp@gmail.com>
2019-05-30 21:14:44 +0800

    removeElement

---
+++ src/main/java/RemoveElement/graceBaoXP/RemoveElement.java
@@ -0,0 +1,22 @@
+package cn.grace.leetcode;
+
+/**
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ *
+ */
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
