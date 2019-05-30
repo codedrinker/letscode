72378980a8c27bd8fe6e5aa906757f77449f2f5d
ZTEer <365165766@qq.com>
2019-05-30 22:57:15 +0800

    Update RemoveElement.java

--- src/main/java/RemoveElement/L07152055/RemoveElement.java
+++ src/main/java/RemoveElement/L07152055/RemoveElement.java
@@ -1,1 +1,15 @@
 
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[j++] = nums[i];
+            } else {
+                length--;
+            }
+        }
+        return length;
+    }
+}
