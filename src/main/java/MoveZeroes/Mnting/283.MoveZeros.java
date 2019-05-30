05d1160ae3b467059048e44156c724db9f31c211
tth <1522986750@qq.com>
2019-05-30 18:08:07 +0800

    修正后

--- src/main/java/MoveZeroes/Mnting/283.MoveZeros.java
+++ src/main/java/MoveZeroes/Mnting/283.MoveZeros.java
@@ -1,13 +1,10 @@
-/*
+﻿/*
  * @lc app=leetcode.cn id=283 lang=java
  *
  * [283] 移动零
  */
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums==null||nums.length == 0){
-            return;
-        }
         int insertPos = 0;
         for(int num:nums){
             if(num!=0)
