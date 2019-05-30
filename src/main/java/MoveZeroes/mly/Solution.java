72e666d6c118054198182602d7225f6567c985c4
maliyuan <liyuan.ma@hpe-airchina.com>
2019-05-30 08:46:31 +0800

    mly -push

---
+++ src/main/java/MoveZeroes/mly/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Created by maliyuan on 2019/5/29.
+ */
+public class MoveZeroes {
+    public void MoveZeroes(int[] nums ){
+        if (nums == null || nums.length == 0 ) {
+            return;
+        }
+        int temp = 0;
+        for (int i =0 ; i < nums.length ; i++) {
+            if(nums[i] != 0 ) {
+                nums[temp] = nums[i];
+                temp++;
+            }
+        }
+        for (int i = temp; i < nums.length ; i++) {
+            nums[i]=0;
+        }
+    }
+}
