a6438600f56d8ed8fb4186e5046deb18622df598
Tenderness1 <496660600@qq.com>
2019-05-30 10:27:09 +0800

    update

---
+++ src/main/java/RemoveElement/Tenderness1/RemoveElement.java
@@ -0,0 +1,20 @@
+package RemoveElement.Tenderness1;
+
+public class RemoveElement {
+    /**
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
