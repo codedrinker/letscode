e6701cd2aca7f838062f53f13b01e9d1c6d742c6
zhangkaijian <zhangkaijian@sinosoft.com.cn>
2019-05-30 11:24:30 +0800

    zhangkaijian123 每日一题 RemoveElement  修改注释 新增借鉴别人的简单方法

--- src/main/java/RemoveElement/zhangkaijian123/RemoveElement.java
+++ src/main/java/RemoveElement/zhangkaijian123/RemoveElement.java
@@ -5,7 +5,7 @@
  * @createdate 2019-05-30 10:05
  **/
 public class RemoveElement {
-
+    //将nums中的val元素全部移动到数组的最后，count记录val出现的次数，nums.length-count就是去掉val后数组的长度
     public static int removeElement(int[] nums, int val) {
         int count = 0;
         for (int i = 0;i<nums.length;i++){
@@ -23,7 +23,16 @@ public class RemoveElement {
         int index = nums.length - count;
         return index;
     }
-
+    //count为去掉val后数组的长度，将val换到数组的最后
+    public int removeElement1(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0;i<nums.length;i++){
+            if (nums[i] != val){
+                nums[count++]=nums[i];
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         int[] nums = new int[]{2,3,3,2};
         int index = removeElement(nums,2);
